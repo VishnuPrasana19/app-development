8644a7864557454ce73cbd55e3bb9ef928527459
VishnuPrasana19 <120376172+VishnuPrasana19@users.noreply.github.com>
2023-10-16 16:27:37 +0530

    Add files via upload

---
+++ App development/Day 24/entity/Cart.java
@@ -0,0 +1,130 @@
+package com.example.demo.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Cart {
+
+    @Id
+    private String id;
+    private String image;
+    private int quantity;
+    private String title;
+    private int price;
+    private String frameType;
+    private String frameSize;
+    private String brands;
+    private int reviews;
+    private String ratings;
+    private boolean bestSeller;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "eyeglass_id")
+    private List<imageDetails> imageDetails;
+
+   
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public List<imageDetails> getImageDetails() {
+        return imageDetails;
+    }
+
+    public void setImageDetails(List<imageDetails> imageDetails) {
+        this.imageDetails = imageDetails;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getFrameType() {
+        return frameType;
+    }
+
+    public void setFrameType(String frameType) {
+        this.frameType = frameType;
+    }
+
+    public String getFrameSize() {
+        return frameSize;
+    }
+
+    public void setFrameSize(String frameSize) {
+        this.frameSize = frameSize;
+    }
+
+    public String getBrands() {
+        return brands;
+    }
+
+    public void setBrands(String brands) {
+        this.brands = brands;
+    }
+
+    public int getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(int reviews) {
+        this.reviews = reviews;
+    }
+
+    public String getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(String ratings) {
+        this.ratings = ratings;
+    }
+
+    public boolean isBestSeller() {
+        return bestSeller;
+    }
+
+    public void setBestSeller(boolean bestSeller) {
+        this.bestSeller = bestSeller;
+    }
+
+   
+}
---
+++ App development/Day 24/service/CartService.java
@@ -0,0 +1,35 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Cart;
+import com.example.demo.repository.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CartService {
+
+    private final CartRepository cartRepository;
+
+    @Autowired
+    public CartService(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
+    public List<Cart> getAllCarts() {
+        return cartRepository.findAll();
+    }
+
+    public Cart getCartById(String id) {
+        return cartRepository.findById(id).orElse(null);
+    }
+
+    public Cart createCart(Cart cart) {
+        return cartRepository.save(cart);
+    }
+
+    public void deleteCart(String id) {
+        cartRepository.deleteById(id);
+    }
+}
